9bfb059698e9a576ef0e623dd6c8835a46f8afa8
Hugo Lecomte <hugo.lecomte@etu.u-bordeaux.fr>
2022-03-17 13:13:35 +0100

    ADD: QTest in CMakeLists.txt, testgui.cpp whiwh is a qt test exemple

---
+++ tests/testgui.cpp
@@ -0,0 +1,38 @@
+#include <QWidget>
+#include <QLineEdit>
+#include <QtTest/QtTest>
+
+#include "../src/image_label.h"
+
+class TestGui: public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+  void testELODIE();
+  void testGui();
+
+};
+
+void TestGui::testELODIE()
+{
+  ImageLabel *imageLabel = new ImageLabel();
+  QLineEdit lineEdit;
+  
+  QTest::keyClicks(&lineEdit, "hello world");
+  
+  QCOMPARE(lineEdit.text(), QString("hello world"));
+}
+
+
+void TestGui::testGui()
+{
+    QLineEdit lineEdit;
+
+    QTest::keyClicks(&lineEdit, "hello world");
+
+    QCOMPARE(lineEdit.text(), QString("hello world"));
+}
+
+QTEST_MAIN(TestGui)
+#include "testgui.moc"
