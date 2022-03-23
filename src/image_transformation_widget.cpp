40e3e85212a7ee9ea91edbd60d6a7bb37562ade8
htrarieux <hugo.trarieux@etu.u-bordeaux.fr>
2022-03-23 15:29:07 +0100

    CHANGE: refactoring in image_transformation_widget.cpp and image_transformation_widget.h

--- src/image_transformation_widget.cpp
+++ src/image_transformation_widget.cpp
@@ -24,8 +24,6 @@ int ImageTransformationWidget::getLayoutCount()
     return this->mainLayout->count();
 }
 
-
-
 MirrorWidget::MirrorWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, MirrorImageTransformation *mirrorImageTransformation)
     : ImageTransformationWidget(mainLayout, parentWidget)
 {
@@ -43,16 +41,18 @@ void MirrorWidget::displayUI(int indexInLayout)
 
     QGridLayout *mirrorLayout = new QGridLayout();
     QLabel *mirrorWidgetTitle = new QLabel("Mirror");
-    mirrorLayout->addWidget(mirrorWidgetTitle, 0, 0, 1, 1);
-    mirrorLayout->addWidget(horizontalMirrorCheckBox, 1, 0, 2, 1);
-    mirrorLayout->addWidget(verticalMirrorCheckBox, 2, 0, 2, 1);
-    mirrorLayout->addWidget(deleteImageTransformationWidgetButton, 0, 1, 1, 1);
+    mirrorLayout->addWidget(mirrorWidgetTitle, ROW_MIRROR_WIDGET_TITLE, COLUMN_MIRROR_WIDGET_TITLE,
+                                                ROW_SPAN_MIRROR_WIDGET_TITLE, COLUMN_SPAN_MIRROR_WIDGET_TITLE);
+    mirrorLayout->addWidget(horizontalMirrorCheckBox, ROW_HORIZONTAL_CHECK_BOX, COLUMN_HORIZONTAL_CHECK_BOX,
+                                                        ROW_SPAN_HORIZONTAL_CHECK_BOX, COLUMN_SPAN_HORIZONTAL_CHECK_BOX);
+    mirrorLayout->addWidget(verticalMirrorCheckBox, ROW_VERTICAL_CHECK_BOX, COLUMN_VERTICAL_CHECK_BOX,
+                                                    ROW_SPAN_VERTICAL_CHECK_BOX, COLUMN_SPAN_VERTICAL_CHECK_BOX);
+    mirrorLayout->addWidget(deleteImageTransformationWidgetButton, ROW_DELETE_WIDGET_BUTTON, COLUMN_DELETE_WIDGET_BUTTON,
+                                                                    ROW_SPAN_DELETE_WIDGET_BUTTON, COLUMN_SPAN_DELETE_WIDGET_BUTTON);
 
     this->mainWidgetGroupBox->setLayout(mirrorLayout);
     this->mainLayout->insertWidget(indexInLayout, mainWidgetGroupBox);
 }
-
-
 
 GrayscaleWidget::GrayscaleWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, GrayscaleImageTransformation *grayscaleImageTransformation)
     : ImageTransformationWidget(mainLayout, parentWidget)
@@ -72,8 +72,6 @@ void GrayscaleWidget::displayUI(int indexInLayout)
     this->mainLayout->insertWidget(indexInLayout, mainWidgetGroupBox);
 }
 
-
-
 AutomaticRotationLymeDataWidget::AutomaticRotationLymeDataWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, AutomaticRotationLymeDataImageTransformation *automaticRotationLymeDataImageTransformation)
     : ImageTransformationWidget(mainLayout, parentWidget)
 {
@@ -92,15 +90,13 @@ void AutomaticRotationLymeDataWidget::displayUI(int indexInLayout)
     this->mainLayout->insertWidget(indexInLayout, mainWidgetGroupBox);
 }
 
-
-
 MorphologicalTransformationWidget::MorphologicalTransformationWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, MorphologicalTransformationImageTransformation *morphologicalTransformationImageTransformation)
     : ImageTransformationWidget(mainLayout, parentWidget)
 {
     this->imageTransformation = morphologicalTransformationImageTransformation;
     this->typeMorphologicalTransformation = new IntegerSlider("Erosion(0), Dilatation(1)", 0, 1);
-    this->kernelSizeSlider = new IntegerSlider("Kernel Size", 0, 20);
-    this->numberIterationMorphologialTransformationSlider = new IntegerSlider("Number of Iteration", 1, 20);
+    this->kernelSizeSlider = new IntegerSlider("Kernel Size", MORPHOLOGICAL_TRANSFORMATION_MINIMUM_KERNEL_SIZE, MORPHOLOGICAL_TRANSFORMATION_MAXIMUM_KERNEL_SIZE);
+    this->numberIterationMorphologialTransformationSlider = new IntegerSlider("Number of Iteration", MORPHOLOGICAL_TRANSFORMATION_MINIMUM_NUMBER_ITERATION, MORPHOLOGICAL_TRANSFORMATION_MAXIMUM_NUMBER_ITERATION);
 
     this->mainWidgetGroupBox->adjustSize();
 }
@@ -113,11 +109,11 @@ void MorphologicalTransformationWidget::displayUI(int indexInLayout)
 
     QGridLayout *morphologicalTransformationLayout = new QGridLayout();
     QLabel *morphologicalTransformationWidgetTitle = new QLabel("Morphological Transformation");
-    morphologicalTransformationLayout->addWidget(morphologicalTransformationWidgetTitle, 0, 0, Qt::AlignLeft);
-    morphologicalTransformationLayout->addWidget(this->typeMorphologicalTransformation, 1, 0, Qt::AlignLeft);
-    morphologicalTransformationLayout->addWidget(this->kernelSizeSlider, 2, 0, Qt::AlignLeft);
-    morphologicalTransformationLayout->addWidget(this->numberIterationMorphologialTransformationSlider, 3, 0, Qt::AlignLeft);
-    morphologicalTransformationLayout->addWidget(deleteImageTransformationWidgetButton, 0, 1, Qt::AlignRight);
+    morphologicalTransformationLayout->addWidget(morphologicalTransformationWidgetTitle, ROW_MORPHOLOGICAL_TRANSFORMATION_WIDGET_TITLE, COLUMN_MORPHOLOGICAL_TRANSFORMATION_WIDGET_TITLE, Qt::AlignLeft);
+    morphologicalTransformationLayout->addWidget(this->typeMorphologicalTransformation, ROW_MORPHOLOGICAL_TRANSFORMATION_TYPE, COLUMN_MORPHOLOGICAL_TRANSFORMATION_TYPE, Qt::AlignLeft);
+    morphologicalTransformationLayout->addWidget(this->kernelSizeSlider, ROW_MORPHOLOGICAL_TRANSFORMATION_KERNEL_SIZE_SLIDER, COLUMN_MORPHOLOGICAL_TRANSFORMATION_KERNEL_SIZE_SLIDER, Qt::AlignLeft);
+    morphologicalTransformationLayout->addWidget(this->numberIterationMorphologialTransformationSlider, ROW_MORPHOLOGICAL_TRANSFORMATION_NUMBER_ITERATION, COLUMN_MORPHOLOGICAL_TRANSFORMATION_NUMBER_ITERATION, Qt::AlignLeft);
+    morphologicalTransformationLayout->addWidget(this->deleteImageTransformationWidgetButton, ROW_MORPHOLOGICAL_TRANSFORMATION_DELETE_WIDGET_BUTTON, COLUMN_MORPHOLOGICAL_TRANSFORMATION_DELETE_WIDGET_BUTTON, Qt::AlignRight);
 
     this->mainWidgetGroupBox->setLayout(morphologicalTransformationLayout);
     this->mainLayout->insertWidget(indexInLayout, mainWidgetGroupBox);
--- src/image_transformation_widget.h
+++ src/image_transformation_widget.h
@@ -10,6 +10,50 @@
 #include <QGroupBox>
 #include <QGridLayout>
 
+//MIRROR WIDGET
+#define ROW_MIRROR_WIDGET_TITLE 0
+#define COLUMN_MIRROR_WIDGET_TITLE 0
+#define ROW_SPAN_MIRROR_WIDGET_TITLE 1
+#define COLUMN_SPAN_MIRROR_WIDGET_TITLE 1
+
+#define ROW_HORIZONTAL_CHECK_BOX 1
+#define COLUMN_HORIZONTAL_CHECK_BOX 0
+#define ROW_SPAN_HORIZONTAL_CHECK_BOX 2
+#define COLUMN_SPAN_HORIZONTAL_CHECK_BOX 1
+
+#define ROW_VERTICAL_CHECK_BOX 2
+#define COLUMN_VERTICAL_CHECK_BOX 0
+#define ROW_SPAN_VERTICAL_CHECK_BOX 2
+#define COLUMN_SPAN_VERTICAL_CHECK_BOX 1
+
+#define ROW_DELETE_WIDGET_BUTTON 0
+#define COLUMN_DELETE_WIDGET_BUTTON 1
+#define ROW_SPAN_DELETE_WIDGET_BUTTON 1
+#define COLUMN_SPAN_DELETE_WIDGET_BUTTON 1
+
+//MORPHOLOGICAL_TRANSFORMATION WIDGET
+#define MORPHOLOGICAL_TRANSFORMATION_MINIMUM_KERNEL_SIZE 1
+#define MORPHOLOGICAL_TRANSFORMATION_MAXIMUM_KERNEL_SIZE 20
+
+#define MORPHOLOGICAL_TRANSFORMATION_MINIMUM_NUMBER_ITERATION 1
+#define MORPHOLOGICAL_TRANSFORMATION_MAXIMUM_NUMBER_ITERATION 10
+
+#define ROW_MORPHOLOGICAL_TRANSFORMATION_WIDGET_TITLE 0
+#define COLUMN_MORPHOLOGICAL_TRANSFORMATION_WIDGET_TITLE 0
+
+#define ROW_MORPHOLOGICAL_TRANSFORMATION_TYPE 1
+#define COLUMN_MORPHOLOGICAL_TRANSFORMATION_TYPE 0
+
+#define ROW_MORPHOLOGICAL_TRANSFORMATION_KERNEL_SIZE_SLIDER 2
+#define COLUMN_MORPHOLOGICAL_TRANSFORMATION_KERNEL_SIZE_SLIDER 0
+
+#define ROW_MORPHOLOGICAL_TRANSFORMATION_NUMBER_ITERATION 3
+#define COLUMN_MORPHOLOGICAL_TRANSFORMATION_NUMBER_ITERATION 0
+
+#define ROW_MORPHOLOGICAL_TRANSFORMATION_DELETE_WIDGET_BUTTON 0
+#define COLUMN_MORPHOLOGICAL_TRANSFORMATION_DELETE_WIDGET_BUTTON 1
+
+
 class ImageTransformationWidget : public QWidget
 {
     public:
@@ -17,13 +61,13 @@ class ImageTransformationWidget : public QWidget
 
         bool isActivated;
         ImageTransformation *imageTransformation;
-        
+
         virtual void displayUI(int indexInLayout) = 0;
         void deleteMainWidgetGroupBox();
 
         QPushButton* getDeleteImageTransformationWidgetButton();
         int getLayoutCount();
-        
+
     protected:
         QVBoxLayout *mainLayout;
         QWidget *parentWidget;
@@ -39,7 +83,7 @@ class MirrorWidget : public ImageTransformationWidget
         MirrorWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, MirrorImageTransformation *mirrorImageTransformation);
 
         void displayUI(int indexInLayout);
-    
+
     private:
         QCheckBox *horizontalMirrorCheckBox;
         QCheckBox *verticalMirrorCheckBox;
@@ -67,7 +111,7 @@ class MorphologicalTransformationWidget : public ImageTransformationWidget
         MorphologicalTransformationWidget(QVBoxLayout *mainLayout, QWidget *parentWidget, MorphologicalTransformationImageTransformation *morphologicalTransformationImageTransformation);
 
         void displayUI(int indexInLayout);
-    
+
     private:
         IntegerSlider *typeMorphologicalTransformation;
         IntegerSlider *kernelSizeSlider;
